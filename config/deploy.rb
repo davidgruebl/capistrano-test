6679716799197434f03e2b716c447946bf07c4d0
David Thomas Grübl <me@davidgruebl.com>
2014-11-04 18:13:56 +0100

    chore: config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'capistrano-test'
+set :repo_url, 'git@github.com:davidgruebl/capistrano-test.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/var/www/capistrano-swag'
 
 # Default value for :scm is :git
 # set :scm, :git
